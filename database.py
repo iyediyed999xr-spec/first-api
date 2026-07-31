afa0f849c166c8d3b9f88c41751d2da8fde339e2
abu_abdellah <iyediyed999xr@gmail.com>
2026-07-31 09:52:04 +0100

    fix: use environment variable for database connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:idk_null@localhost:5432/first_api_db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
